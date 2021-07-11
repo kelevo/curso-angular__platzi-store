20bef06986b42355c7bde4e868b58de6d48871ef
kelevo <patrickjhonatanh@gmail.com>
2021-07-10 23:53:42 -0500

    Metodo POST y GET en el proyecto

--- src/app/core/services/products/products.service.ts
+++ src/app/core/services/products/products.service.ts
@@ -1,67 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-import { Product } from '../../models/product.model';
+import { Product } from './../../models/product.model';
+
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  // Trabajando con objetos
-  products: Product[ ] = [
-    {
-      id: '1',
-      image: 'assets/images/camiseta.png',
-      title: 'Camiseta',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '2',
-      image: 'assets/images/hoodie.png',
-      title: 'Hoodie',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '3',
-      image: 'assets/images/mug.png',
-      title: 'Mug',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '4',
-      image: 'assets/images/pin.png',
-      title: 'Pin',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '5',
-      image: 'assets/images/stickers1.png',
-      title: 'Stickers',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '6',
-      image: 'assets/images/stickers2.png',
-      title: 'Stickers',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    }
-  ];
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  constructor() { }
-
-  // Devuelve todos los productos
   getAllProducts() {
-    return this.products;
+    return this.http.get<Product[]>(`${environment.url_api}/products/`);
   }
 
-  // Devuelve el producto que coincida con el id
   getProduct(id: string) {
-    return this.products.find(item => id === item.id)
+    return this.http.get<Product>(`${environment.url_api}/products/${id}`);
+  }
+
+  createProduct(product: Product) {
+    return this.http.post(`${environment.url_api}/products/`, product);
   }
 }
--- src/app/product/components/product-detail/product-detail.component.ts
+++ src/app/product/components/product-detail/product-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
 import { ProductsService } from '../../../core/services/products/products.service';
-import { Product } from '../../../core/models/product.model';
+import { Product } from 'src/app/core/models/product.model';
 
 @Component({
   selector: 'app-product-detail',
@@ -22,8 +22,34 @@ export class ProductDetailComponent implements OnInit {
     // Recibimos datos
     this.route.params.subscribe((params: Params) => {
       const id = params.id;
-      this.product = this.productsService.getProduct(id);
+      this.fetchProduct(id);
+      //this.product = this.productsService.getProduct(id);
     });
   }
+
+  fetchProduct(id: string) {
+    this.productsService.getProduct(id)
+    .subscribe(product => {
+      this.product = product;
+    })
+  }
+
+  createProduct() {
+
+    const newProduct: Product = {
+      id: '2222',
+      title: 'nuevo desde angular',
+      image: 'assets/images/stickers1.png',
+      price: 3000,
+      description: 'nuevo producto'
+    }
+
+    this.productsService.createProduct(newProduct)
+    .subscribe(product => {
+      console.log(product);
+    })
+  }
+
+
 
 }
