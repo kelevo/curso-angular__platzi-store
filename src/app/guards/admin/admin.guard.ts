1066e7e42ce981b02ef31b35c566e89df77ab493
kelevo <patrickjhonatanh@gmail.com>
2021-07-16 20:47:09 -0500

    Aplicando guardianes

--- src/app/guards/admin/admin.guard.ts
+++ src/app/guards/admin/admin.guard.ts
@@ -1,15 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.authService.hasUser().pipe(
+      map(user => user === null ? false : true),
+      tap(hasUser => {
+        if (!hasUser) {
+          this.router.navigate(['/auth/login']);
+        }
+      })
+    );
   }
-  
 }
